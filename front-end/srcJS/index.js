cabc6e8f606894c5436d2438370a7f413826f2f8
sekoudosso82 <sekou.dosso82@gmail.com>
2020-03-26 16:09:45 -0400

    reviews.js code

--- front-end/srcJS/index.js
+++ front-end/srcJS/index.js
@@ -1,12 +1,25 @@
+// import { fuchsia } from "color-name"
 
 let div = document.querySelector("div")
 const ITEM_BASE_URL = "http://localhost:3000/api/v1/items"
 
 document.addEventListener('DOMContentLoaded', function(){
   getItems()
+  // getReviews()
 })
 
+function getItems() {
+  fetch(ITEM_BASE_URL)
+  .then(response => response.json())
+  .then(items => {
+    items.forEach(function(item){
+      addItem(item)
+    })
+  })
+} 
+
 function addItem(item){
+  // console.log()
   const itemDiv = document.createElement('div')
 
   itemDiv.className = "item-div"
@@ -18,20 +31,45 @@ function addItem(item){
     <img class="itemImg" alt="" src=${item.imageUrl} />
     <button data-purpose="delete">Delete</button>
     <button class="">Update</button>
-    <button> Review </button>
+    <button>Review</button>
+    <ul data-id=${item.id}> </ul>
   `
-  div.append(itemDiv)
+  div.append(itemDiv) 
+  // let ul = document.getElementById('reviews')
+  let ul = document.querySelector(`[data-id='${item.id}']`)
+  // ul.dataset.id = item.id
+  getReviews(ul)
 }
 
-function getItems() {
-  fetch(ITEM_BASE_URL)
+function getReviews(ul) {
+  fetch('http://localhost:3000/api/v1/reviews')
   .then(response => response.json())
-  .then(items => {
-    items.forEach(function(item){
-      addItem(item)
+  .then(reviews => {
+    console.log(reviews)
+    reviews.forEach(function(review){
+     addReview(ul, review)
     })
+    
   })
 } 
+
+function addReview(ul, review)
+{
+    // let ul = document.getElementById('reviews')
+    let li = document.createElement('li')
+    li.innerHTML = ` <h2>${review.comment}</h2>`
+    if (review.item_id == ul.dataset.id) {
+      ul.appendChild(li)
+    }
+   
+}
+
+
+
+// get reviews
+
+
+
 
 
 
--- front-end/srcJS/update.js
+++ front-end/srcJS/update.js
@@ -1,31 +1,31 @@
-
-// update item 
-let divv = document.querySelector("div")
+// update item
+let divv = document.querySelector('div')
 document.addEventListener('click', function(){
-    if (event.target.innerText === "Update"){
-        console.log ("update")
+    if (event.target.innerText === 'Update'){
+        console.log ('update')
         updateItem(event.target)
-    }       
+    }
 })
-
 function updateItem(button) {
     let id = button.parentNode.dataset.id
-    
-    let newForm = document.createElement("form")
+    console.log(button.parentNode)
+    let newForm = document.createElement('form')
     newForm.innerHTML = `
       <label>name:</label>
-      <input type="text" name="name" />
+      <input type='text' name='name' />
       <br/>
       <label>description:</label>
-      <input type="text" name="description" />
+      <input type='text' name='description' />
       <br/>
       <label>imageUrl:</label>
-      <input type="text" name="imageUrl" />
+      <input type='text' name='imageUrl' />
       <br/>
-      <input type="submit" />
+      <input class="subbnt" type="submit" value="Submit">
+
     `
+    button.replaceWith(newForm)
     const itemDiv = document.createElement('div')
-    itemDiv.className = "item-div"
+    itemDiv.className = 'item-div'
     itemDiv.append(newForm)
     document.body.prepend(itemDiv )
     newForm.addEventListener('submit', function(event){
@@ -33,25 +33,32 @@ function updateItem(button) {
       let name = event.target.name.value
       let description = event.target.description.value
       let imageUrl = event.target.imageUrl.value
-      
       let newItem = {name: name, description: description, imageUrl: imageUrl}
-   
       itemDiv.remove()
-     
       updateItemDb(newItem, id)
-    })  
+    })
 }
-
-function updateItemDb(newi, i)
+function updateItemDb(newItem, id)
 {
-    fetch(`http://localhost:3000/api/v1/items/${i}`, {
-            method: "PATCH",
+    fetch(`http://localhost:3000/api/v1/items/${id}`, {
+            method: 'PATCH',
             headers: {
-            "content-type": "application/json",
-            "accept": "application/json"
+            'content-type': 'application/json',
+            'accept': 'application/json'
             },
-            body: JSON.stringify({name: newi.name, description: newi.description, imageUrl: newi.imageUrl}) 
+            body: JSON.stringify({name: newItem.name, description: newItem.description, imageUrl: newItem.imageUrl})
     })
     .then(response => response.json())
-    .then(console.log)               
+    .then(item => {
+        let divItem = document.querySelectorAll(`[data-id='${id}']`)
+        console.log(divItem)
+        divItem[0].innerHTML=`
+        <h3>${item.name}</h3>
+        <h3>${item.description}</h3>
+        <img class='itemImg' alt='' src=${item.imageUrl} />
+        <button data-purpose='delete'>Delete</button>
+        <button class=''>Update</button>
+        <button> Review </button>
+        <ul data-id=${item.id}> </ul>`
+    })
 }
